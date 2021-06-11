ff8fc450b11148c1ea6c45114e12c7e5dcdb6985
gabe-morris <gabe.e.morris@gmail.com>
2021-06-11 12:47:43 -0500

    finished js task 6

--- index.js
+++ index.js
@@ -285,9 +285,20 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/){
-    /*Your Code Here*/
-  }
+function addArtist(array){
+ const newArtist = {
+  id: 20,
+  name: "Gabe Morris", 
+  years: "1997-June 11th,2021",
+  genre: "Web Design", 
+  nationality: "American",
+  bio: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit deserunt earum sed ratione illo aperiam commodi corrupti a impedit consequuntur."
+  } 
+  array.push(newArtist);
+  return array;
+}
+console.log(addArtist(artists))
+
 
   
 
